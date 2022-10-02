7b0cc57a9afe55810cfc35b80a9aeb3363803af1
Annemarie Sheridan <atsheridan38@gmail.com>
2022-10-02 12:47:54 -0700

    message

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function generatePassword() {
   var passwordNumbers = confirm ("Do you want numbers?")
   var passwordUppercase = confirm ("Do you want uppercase characters?")
   var passwordLowercase = confirm ("Do you want lowercase characters?")
+}
 
   var userInfo = {
     passwordLength: passwordLength,
@@ -24,39 +25,46 @@ function generatePassword() {
     passwordLowercase: passwordLowercase,
   }
   console.log(userInfo)
+
+
 // make an array-multiple arrays for all of these options
 
 if(userInfo.passwordUppercase) { 
   console.log(megArray)
   megArray = megArray + passwordUppercase
   console.log(megArray)
+}
 
   if(userInfo.passwordLowercase) { 
     console.log(megArray)
     megArray = megArray + passwordLowercase
     console.log(megArray)
+  }
 
     if(userInfo.passwordLength) { 
       console.log(megArray)
       megArray = megArray + passwordLength 
       console.log(megArray)
+    }
 
       if(userInfo.passwordNumbers) { 
         console.log(megArray)
         megArray = megArray + passwordNumbers 
         console.log(megArray)
+      }
 
         if(userInfo.passwordSpecialCharacters) { 
           console.log(megArray)
           megArray = megArray + passwordSpecialCharacters
           console.log(megArray)
+        }
 
   // add an if statement for all of the arrays
-}
+
   
   // return num1 + num2
   // console.log("after")
- } 
+ 
 
  
 var generateBtn = document.querySelector("#generate");
